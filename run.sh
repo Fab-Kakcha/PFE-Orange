024f61eaefb1f29511f90ca1d3607cd61568f378
Fabrice Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-03-24 18:21:25 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+export OMSGATEWAY_HOME=/opt/application/testlab/utils/stageFabrice
+export CLASSPATH=$OMSGATEWAY_HOME:$OMSGATEWAY_HOME/lib/java_websocket.jar:$OMSGATEWAY_HOME/lib/log4j-1.2.9.jar:$OMSGATEWAY_HOME/lib/commons-logging-1.1.jar:$OMSGATEWAY_HOME/lib/gson-2.3.jar
+java com.orange.olps.stageFabrice.webrtc.MonServiceWtc
