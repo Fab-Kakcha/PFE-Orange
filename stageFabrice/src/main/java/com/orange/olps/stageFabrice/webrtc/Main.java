194f2e18218129bf8a140ae546a98d106a0791dc
Fab-Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-03-24 22:54:28 +0100

    commit

---
+++ stageFabrice/src/main/java/com/orange/olps/stageFabrice/webrtc/Main.java
@@ -0,0 +1,12 @@
+package com.orange.olps.stageFabrice.webrtc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
