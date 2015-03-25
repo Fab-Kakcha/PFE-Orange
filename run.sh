9d7e46b5574e198bd21a85d9d3f8e34770ea5fb5
Fabrice Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-03-25 16:20:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export OMSGATEWAY_HOME=/opt/testlab/utils/stageFabriceNonMaven
+export OMSGATEWAY_HOME=/opt/testlab/utils/stageFabrice/src/main/java
 export CLASSPATH=.:$OMSGATEWAY_HOME:$OMSGATEWAY_HOME/lib/java_websocket.jar:$OMSGATEWAY_HOME/lib/log4j-1.2.9.jar:$OMSGATEWAY_HOME/lib/commons-logging-1.1.jar:$OMSGATEWAY_HOME/lib/gson-2.3.jar
-java stageFabriceNonMaven.MonServiceWtc
+java com.orange.olps.stageFabrice.webrtc.MonServiceWtc
