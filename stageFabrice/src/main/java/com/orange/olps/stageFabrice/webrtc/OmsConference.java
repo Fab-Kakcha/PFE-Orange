1662ab2efaa76090943f830dbcc7307af43d236a
Fabrice Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-03-27 13:18:42 +0100

    Update OmsConference.java

--- stageFabrice/src/main/java/com/orange/olps/stageFabrice/webrtc/OmsConference.java
+++ stageFabrice/src/main/java/com/orange/olps/stageFabrice/webrtc/OmsConference.java
@@ -3,17 +3,12 @@
  */
 package com.orange.olps.stageFabrice.webrtc;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -59,12 +54,25 @@ public class OmsConference {
 		
 		confName = name;
 		connOMSConf = new VipConnexion(hostVipConf, portVipConf);
-		String respCreation = connOMSConf.getReponse("<conference> <create requestid=\"req1\" conferenceid=\"" + 
+		/*String respCreation = connOMSConf.getReponse("<conference> <create requestid=\"req1\" conferenceid=\"" + 
 		confName + "\" /></conference>" );
 		
 		if (respCreation.indexOf("OK") == -1)
-			throw new OmsException("Error cannot create the conference : "+ respCreation);		
+			throw new OmsException("Error cannot create the conference : "+ respCreation);*/
 	}
+	
+	public void create() throws OmsException{
+		
+		String confName = getConfName();
+		
+		String respCreation = connOMSConf.getReponse("<conference> <create requestid=\"req1\" conferenceid=\"" + 
+				confName + "\" /></conference>" );
+				
+		if (respCreation.indexOf("OK") == -1)
+				throw new OmsException("Error cannot create the conference : "+ respCreation);
+	}
+	
+	
 	
 	/**
 	 * 
