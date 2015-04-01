c516d6f7cd0c536c2c02a56b5623a67dd14359cd
Fabrice Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-04-01 11:35:43 +0200

    Update compile.sh

--- Test/src/compile.sh
+++ Test/src/compile.sh
@@ -1,4 +1,4 @@
 export OMSGATEWAY_HOME=/opt/testlab/utils/Test/src
-export CLASSPATH=.:$OMSGATEWAY_HOME:$OMSGATEWAY_HOME/lib/java_websocket.jar:$OMSGATEWAY_HOME/lib/log4j-1.2.9.jar:$OMSGATEWAY_HOME/lib/commons-logging-1.1.jar:$OMSGATEWAY_HOME/lib/gson-2.3.jar:$OMSGATEWAY_HOME/lib/oms-api-webrtc.jar
+export CLASSPATH=.:$OMSGATEWAY_HOME:$OMSGATEWAY_HOME/lib/java_websocket.jar:$OMSGATEWAY_HOME/lib/log4j-1.2.9.jar:$OMSGATEWAY_HOME/lib/commons-logging-1.1.jar:$OMSGATEWAY_HOME/lib/gson-2.3.jar:$OMSGATEWAY_HOME/lib/api-oms-webrtc.jar
 rm test/*.class
 javac test/*.java
