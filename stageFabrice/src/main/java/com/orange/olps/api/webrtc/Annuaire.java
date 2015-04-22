17e7b55e044c01a2ce92a26eff5878cafefcaede
Fabrice Kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-04-22 11:45:35 +0200

    Update Annuaire.java

--- stageFabrice/src/main/java/com/orange/olps/api/webrtc/Annuaire.java
+++ stageFabrice/src/main/java/com/orange/olps/api/webrtc/Annuaire.java
@@ -24,9 +24,9 @@ public class Annuaire {
 	}
 	
 	/**
-	 * Check if the userName already exists or not in the annuaire. Will send a message to the client/Browser
-	 * if the userName already exists. The message sent to the client/Browser when the userName
-	 * exists is CilentWebSocket.send("userNameExist").
+	 * Check whether or not the userName provided by the OmsCall/Client already exists in the annuaire. 
+	 * A message will be sent to the client/Browser
+	 * if the userName already exists. That message is CilentWebSocket.send("userNameExist").
 	 * @param call OmsCall/Client/Browser who is trying to connect to OMS
 	 * @param userName userName provided by OmsCall/Client/Browser when connecting to OMS
 	 * @return true if the userName already exists, so a message is sent through the WebSocket to the 
@@ -40,8 +40,7 @@ public class Annuaire {
 		else if(userName==null)
 			throw new IllegalArgumentException("Argument String cannot be null");
 		
-		if (annuaire.containsValue(userName)){
-			
+		if (annuaire.containsValue(userName)){		
 			call.getWebSocket().send("userNameExist");
 			return true;
 		}
@@ -86,14 +85,14 @@ public class Annuaire {
 	
 	
 /**
- * To show the name of OmsCall/Client/Browser currently connected to OMS. 
+ * To show userNames of all OmsCall/Client/Browser currently connected to OMS. 
  * @param call the latest OmsCall/Client/Browser to connect to OMS, or the call who is to leave OMS. 
  * His userName will be sent to other already connected to OMS, and it will received the userName 
  * of the latter when connecting, or he will only sent his userName to others when disconnecting
  * userNames are send through WebSocket.
  * @param bool true if the parameter OmsCall/Client/Browser has connected to OMS, then 
  *  WebSocket.send("showUserNameConnectedtoOMS:"+userName) is sent to others Clients, so they can 
- *  print out his name. He will print out theirs userNames as well.
+ *  print out his name. He will print out the latter's userNames as well.
  * and false if OmsCall/Client/Browser has disconnected from OMS, then 
  *  WebSocket.send("deleteUserName:"+userName) is sent to others clients, so they can remove his userName from 
  *  the list of people connected to OMS
@@ -104,60 +103,59 @@ public class Annuaire {
 		if(call == null)
 			throw new IllegalArgumentException("Argument OmsCall cannot be null");
 		
-		if(!annuaire.isEmpty()){
-			
-			if(annuaire.containsKey(call.getWebSocket())){
-				
-				Set<WebSocket> listCli = annuaire.keySet();
-				Iterator<WebSocket> ite = listCli.iterator();
-				String userName;
-				String userName2 = annuaire.get(call.getWebSocket());
-				
-				Collection<String> c = annuaire.values();
-				Iterator<String> ite1;
-				
-				if(bool){
-							
-					while(ite.hasNext()){
-						
-						WebSocket ws = ite.next();
-						ite1 = c.iterator();
-						while(ite1.hasNext()){
-							
-							userName = ite1.next();				
-							if(ws == call.getWebSocket()){					
-								if(userName2 != userName)
-									ws.send("showUserNameConnectedToOMS:"+userName);
-								
-							}else{
-								if(userName2 == userName)
-								ws.send("showUserNameConnectedToOMS:"+userName);
-							}				
-						}			
+		
+		if (annuaire.containsKey(call.getWebSocket())) {
+
+			Set<WebSocket> listCli = annuaire.keySet();
+			Iterator<WebSocket> ite = listCli.iterator();
+			String userName;
+			String userName2 = annuaire.get(call.getWebSocket());
+
+			Collection<String> c = annuaire.values();
+			Iterator<String> ite1;
+
+			if (bool) {
+
+				while (ite.hasNext()) {
+
+					WebSocket ws = ite.next();
+					ite1 = c.iterator();
+					while (ite1.hasNext()) {
+
+						userName = ite1.next();
+						if (ws == call.getWebSocket()) {
+							if (userName2 != userName)
+								ws.send("showUserNameConnectedToOMS:" + userName);
+
+						} else {
+							if (userName2 == userName)
+								ws.send("showUserNameConnectedToOMS:" + userName);
+						}
 					}
-					
-				}else{
-					
-					while(ite.hasNext()){
-						
-						WebSocket ws = ite.next();
-						ite1 = c.iterator();
-						
-						if(ws != call.getWebSocket()){					
-								ws.send("deleteUserName:"+userName2);					
-						}						
-					}
-					
-					annuaire.remove(call.getWebSocket());
 				}
-			}else 
-				throw new OmsException("cannot show people connected to OMS. OmsCall/Client/Browser is "
-						+ " not in the annuaire");		
-		}		
+
+			} else {
+
+				while (ite.hasNext()) {
+
+					WebSocket ws = ite.next();
+					ite1 = c.iterator();
+
+					if (ws != call.getWebSocket()) {
+						ws.send("deleteUserName:" + userName2);
+					}
+				}
+
+				annuaire.remove(call.getWebSocket());
+			}
+		} else
+			throw new OmsException(
+					"cannot show people connected to OMS. OmsCall/Client/Browser is"
+							+ " not in the annuaire");	
 	}
 	
 	/**
-	 * Updating param. Normally param is in form of "confName:mode". But we need to add the userName given by 
+	 * Updating param, normally param is in form of "confName:mode", but we need to add the userName given by 
 	 * the client when connecting to OMS. This userName will be concatenated to param, and no need for the client 
 	 * to provide its userName twice. The userName he previously provided is taken from the annuaire.
 	 * @param call the OmsCall/Client/Browser connected to OMS
@@ -166,7 +164,7 @@ public class Annuaire {
 	 * @return new value of String param in the form of "userName:confName:mode" is returned if the user is
 	 * found in the annuaire and null is returned otherwise.
 	 */
-	public String updatingParam(OmsCall call, String param){
+	public String updatingStringParam(OmsCall call, String param){
 		
 		if(call == null)
 			throw new IllegalArgumentException("Argument OmsCall cannot be null");
@@ -183,17 +181,11 @@ public class Annuaire {
 			return null;
 	}
 	
+	
 	/**
-	 * A OmsCall enters a conference. So his userName should no longer appear into the list of people connected
-	 * to OMS, rather to the list of people in a conference
+	 * To get the annuaire 
+	 * @return the annuaire
 	 */
-	public void fromConnectedToOmsToInConf(OmsCall call){
-	
-		if(call == null)
-			throw new IllegalArgumentException("Argument OmsCall cannot be null");
-	}
-	
-	
 	public HashMap<WebSocket, String> getAnnuaire(){
 		 return annuaire;
 	}
