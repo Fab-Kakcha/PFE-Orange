25a0d10252807f5238bcba5b928316af729436ee
fabrice-kakcha <fkakchantichi@enseirb-matmeca.fr>
2015-06-15 19:28:05 +0200

    commit

--- Test/src/run.sh
+++ Test/src/run.sh
@@ -1,3 +1,5 @@
 export OMSGATEWAY_HOME=/opt/testlab/utils/Test/src
 export CLASSPATH=.:$OMSGATEWAY_HOME:$OMSGATEWAY_HOME/lib/java_websocket.jar:$OMSGATEWAY_HOME/lib/log4j-1.2.9.jar:$OMSGATEWAY_HOME/lib/commons-logging-1.1.jar:$OMSGATEWAY_HOME/lib/gson-2.3.jar:$OMSGATEWAY_HOME/lib/api-oms-webrtc.jar:$OMSGATEWAY_HOME/lib/org.json.jar:$OMSGATEWAY_HOME/lib/api-oms-sip.jar
-java test.MonServiceSip
+rm test/*.class
+javac test/*.java
+java test.MonServiceSip $1
